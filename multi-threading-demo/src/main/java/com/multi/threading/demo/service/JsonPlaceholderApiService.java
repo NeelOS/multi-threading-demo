f4d85ac0efc11989cb976fa47c7de9c587764dae
NeelOS <iiest.indraneel@gmail.com>
2024-12-06 15:30:50 +0530

    sequence processing

--- multi-threading-demo/src/main/java/com/multi/threading/demo/service/JsonPlaceholderApiService.java
+++ multi-threading-demo/src/main/java/com/multi/threading/demo/service/JsonPlaceholderApiService.java
@@ -1,25 +1,37 @@
 package com.multi.threading.demo.service;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.multi.threading.demo.client.JsonPlaceholderApiWebClient;
+import com.multi.threading.demo.model.GetPostByIdApiRes;
 import com.multi.threading.demo.model.GetPostByIdReq;
 import com.multi.threading.demo.model.ResponseObjectData;
 
-import io.micrometer.common.util.StringUtils;
-
 @Service
 public class JsonPlaceholderApiService {
-	
+
 	@Autowired
 	private JsonPlaceholderApiWebClient jsonPlaceholderApiWebClient;
 
 	public ResponseObjectData getPostById(final GetPostByIdReq apiRequest) {
 		final ResponseObjectData response = new ResponseObjectData();
-		if(StringUtils.isNotEmpty(apiRequest.getPostIds())) {
-			final String postid = apiRequest.getPostIds();
-			response.setData(jsonPlaceholderApiWebClient.getPostById(postid));
+		final List<GetPostByIdApiRes> postByidResponses = new ArrayList<>();
+		if (StringUtils.isNotEmpty(apiRequest.getPostIds())) {
+			final List<String> postids = Arrays.asList(StringUtils.split(apiRequest.getPostIds(), ","));
+			if (apiRequest.isDoMultiThreading()) {
+
+			} else {
+				postids.forEach(postid -> {
+					postByidResponses.add(jsonPlaceholderApiWebClient.getPostById(postid));
+				});
+			}
+			response.setData(postByidResponses);
 		}
 		return response;
 	}
